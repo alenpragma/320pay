9f3964170204e718398c8cfcd3078419f243af0b
Remon Tripura <remontripura045@gmail.com>
2024-07-27 17:46:46 +0600

    update

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,10 +1,10 @@
 import { createBrowserRouter } from "react-router-dom"
 import App from "../App"
-import Home from "../pages/Home"
-import About from "../pages/About"
-import GeneralSettings from "../comonents/GeneralSettings"
-import Dashboard from "../pages/Dashboard/Dashboard"
 import StartHere from "../pages/StartHere/StartHere"
+import Deposit from "../pages/Deposit/Deposit"
+import Dashboard from "../pages/Dashboard/Dashboard"
+import Licenses from "../pages/Licenses/Licenses"
+import Orders from "../pages/Orders/Orders"
 
 const routes = createBrowserRouter([
   {
@@ -13,37 +13,35 @@ const routes = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <Home />,
-      },
-      {
-        path: "/about",
-        element: <About />,
-      },
-
-      {
-        path: "/dashboard",
         element: <Dashboard />,
       },
-
       {
-        path: "/startHere",
+        path: '/start-here',
         element: <StartHere />,
       },
       {
-        path: "/generalSettings",
-        element: <GeneralSettings />,
+        path: "/deposit",
+        element: <Deposit />,
+      },
+      {
+        path: "/licenses",
+        element: <Licenses />,
+      },
+      {
+        path: "/orders",
+        element: <Orders />,
       },
     ],
   },
 
-  {
-    path: "/signup",
-    element: <p>Signup</p>,
-  },
-  {
-    path: "*",
-    element: <p>NOT FOUND</p>,
-  },
+  // {
+  //   path: "/signup",
+  //   element: <p>Signup</p>,
+  // },
+  // {
+  //   path: "*",
+  //   element: <p>NOT FOUND</p>,
+  // },
 ])
 
 export default routes
