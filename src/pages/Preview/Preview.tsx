53c97b4722f32f5ffc6c990736459f3589c56759
Remon Tripura <remontripura045@gmail.com>
2024-09-14 21:20:42 +0600

    update

--- src/pages/Preview/Preview.tsx
+++ src/pages/Preview/Preview.tsx
@@ -1,44 +1,44 @@
-import { useLocation, useNavigate } from "react-router-dom"
-import { useState } from "react"
-import axiosInstance from "../../utils/axiosConfig"
-import { toast } from "react-toastify"
-import LoaingAnimation from "../../Components/Loading/LoaingAnimation"
+import { useLocation, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import axiosInstance from "../../utils/axiosConfig";
+import { toast } from "react-toastify";
+import LoaingAnimation from "../../Components/Loading/LoaingAnimation";
 
 const Preview = () => {
-  const navigate = useNavigate()
-  const location = useLocation()
-  const [loading, setLoading] = useState(false)
-  const { withdrawData } = location.state || {}
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [loading, setLoading] = useState(false);
+  const { withdrawData } = location.state || {};
 
-  console.log(withdrawData)
+  console.log(withdrawData);
 
   const confirmWithdrow = async () => {
     const confirmData = {
       amount: withdrawData.amount,
       token_id: withdrawData.token_id,
       wallet_address: withdrawData.wallet_address,
-    }
-    setLoading(true)
+    };
+    setLoading(true);
     const withdrowResponse = await axiosInstance.post(
       "/client/withdraw",
       confirmData
-    )
-    console.log(withdrowResponse)
+    );
+    console.log(withdrowResponse);
 
     if (withdrowResponse?.data?.success == 200) {
-      setLoading(false)
-      toast.success(withdrowResponse?.data?.msg)
-      const { ...confirmationResponsData } = withdrowResponse.data
+      setLoading(false);
+      toast.success(withdrowResponse?.data?.msg);
+      const { ...confirmationResponsData } = withdrowResponse.data;
 
       navigate("/dashboard/withdraw/preview/otp", {
         state: { confirmationResponsData },
-      })
+      });
     }
     if (withdrowResponse?.data?.error) {
-      toast.error(withdrowResponse?.data?.messsage)
-      setLoading(false)
+      toast.error(withdrowResponse?.data?.messsage);
+      setLoading(false);
     }
-  }
+  };
 
   return (
     <div className="md:w-1/2 w-full mx-auto mt-20 rounded-lg px-3">
@@ -68,7 +68,7 @@ const Preview = () => {
         <div className="flex items-center justify-between">
           <h6 className="text-secondary text-[14px]">Withdrawal Amount</h6>
           <h6 className=" text-[14px] px-2 text-secondary py-[2px] w-fit">
-            ${withdrawData?.amount || 0}
+            ${withdrawData?.amount - (withdrawData?.amount / 100) * 0.5}
           </h6>
         </div>
         <div className="flex items-center justify-between">
@@ -89,7 +89,7 @@ const Preview = () => {
         </button>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default Preview
+export default Preview;
